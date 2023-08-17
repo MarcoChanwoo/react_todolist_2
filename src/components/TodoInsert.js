840fb5902458e2539a907a51f2b3bcc61f203a75
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-17 23:57:24 +0900

    3rd update

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,32 @@
+import { useState, useCallback } from 'react';
 import { MdFlightTakeoff } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback((e) => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        (e) => {
+            onInsert(value);
+            setValue(''); // value 값 초기화
+            // submit 이벤트는 브라우저에서 새로고침을 발생시키므로
+            // 이를 방지하기 위해 아래 함수를 호출함
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요" />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdFlightTakeoff />
             </button>
